7735891100eded8f6f96caced2b531049b1c9541
Sebastian Reategui <seb.reategui@gmail.com>
2025-02-28 18:01:35 +1100

    remove pseudocode (lol) to convert types to strings - TODO

--- go/libale/libale.consts.go
+++ go/libale/libale.consts.go
@@ -12,41 +12,12 @@ const ALEDataWord = "Data"
 // ALEHeadingWordPattern represents regexp for this section
 const ALEHeadingWordPattern = `(?ms)^Heading(\r?\n|\r)(?P<fields>.*)(\r?\n|\r)(\r?\n|\r)Column(\r?\n|\r)(?P<columns>.*)(\r?\n|\r)(\r?\n|\r)Data$`
 
-
-// String returns the string value
-func (fd ALEFieldDelimiter) String() string {
-	if fd.Value == "TABS" {
-		return "TABS"
-	} else {
-		if LIBALE_OUTPUT_STRICT_VALUES {
-			// For greater compatibility, always specify tabs.
-			return "TABS"
-		} else {
-			return fd.Value
-		}
-	}
-}
-
 // ALE video formats
 //const (
 //	ALE_VIDEO_FORMAT_CUSTOM ALEVideoFormat = "CUSTOM"
 //	ALE_VIDEO_FORMAT_1080   ALEVideoFormat = "1080"
 //)
-
-// String returns the string value
-func (vf ALEVideoFormat) String() string {
-	if vf.Value == "1080" {
-		return vf.Value
-	} else if vf.Value == "CUSTOM" {
-		return "CUSTOM"
-	} else {
-		if LIBALE_OUTPUT_STRICT_VALUES {
-			// For greater compatibility, return CUSTOM.
-			return "CUSTOM"
-		}
-	}
-	return vf.Value
-}
+// TODO: add PAL, NTSC, and gather other possibilities
 
 // ALE audio formats
 //const (
@@ -54,23 +25,7 @@ func (vf ALEVideoFormat) String() string {
 //	ALE_AUDIO_FORMAT_48KHZ  ALEAudioFormat = "48kHz"
 //	ALE_AUDIO_FORMAT_NONE   ALEAudioFormat = "NONE"
 //)
-
-// String returns the string value
-func (af ALEAudioFormat) String() string {
-	if af.Value == "48kHz" {
-		return "48kHz"
-	} else if af.Value == "CUSTOM" {
-		return "CUSTOM"
-	} else if af.Value == "NONE" {
-		return "NONE"
-	} else {
-		if LIBALE_OUTPUT_STRICT_VALUES {
-			// For greater compatibility, return CUSTOM.
-			return "CUSTOM"
-		}
-	}
-	return af.Value
-}
+// TODO: add 44khz and others
 
 // ALE frame rates
 //const (
@@ -89,34 +44,6 @@ func (af ALEAudioFormat) String() string {
 //	ALE_FRAME_RATE_23976
 //)
 
-// String returns the string value
-func (fr ALEFrameRate) String() string {
-	switch fr.Value {
-	case "60":
-		return "60"
-	case "59.94NDF":
-		return "59.94NDF"
-	case "59.94DF":
-		return "59.94DF"
-	case "50":
-		return "50"
-	case "48":
-		return "48"
-	case "30NDF":
-		return "30NDF"
-	case "30DF":
-		return "30DF"
-	case "29.97NDF":
-		return "29.97NDF"
-	case "29.97DF":
-		return "29.97DF"
-	case "25":
-		return "25"
-	case "24":
-		return "24"
-	case "23.976":
-		return "23.976"
-	default:
-		return "CUSTOM"
-	}
-}
+// TODO: HEADER FIELDS TO ADD
+// 'TAPE' - string, any custom string tape name
+// 'FILM_FORMAT' - string, "35 mm"
