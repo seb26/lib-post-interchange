c7cf3544198e528820ca399f4730cfa0878be3cc
Sebastian Reategui <seb.reategui@gmail.com>
2025-02-28 18:16:51 +1100

    fmt

--- go/libale/settings.go
+++ go/libale/settings.go
@@ -5,6 +5,4 @@ package libale
 // fields is used, instead of the actual user defined value. This will prevent
 // a failure when opening the file in legacy applications that do not expect
 // the range of possible values a user could specify using this library.
-const (
-	LIBALE_OUTPUT_STRICT_VALUES = true
-)
+const LIBALE_OUTPUT_STRICT_VALUES = true
