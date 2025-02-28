765c96708c749d82daf46c10865b39e6d3998c80
Sebastian Reategui <seb.reategui@gmail.com>
2025-02-28 18:00:36 +1100

    move to settings

--- go/libale/libale.consts.go
+++ go/libale/libale.consts.go
@@ -1,14 +1,5 @@
 package libale
 
-// LIBALE_OUTPUT_STRICT_VALUES is a runtime flag to control certain values.
-// By default (true), the most compatible and generic value for the following
-// fields is used, instead of the actual user defined value. This will prevent
-// a failure when opening the file in legacy applications that do not expect
-// the range of possible values a user could specify using this library.
-const (
-	LIBALE_OUTPUT_STRICT_VALUES = true
-)
-
 // ALEHeadingWord represents the first word in the ALE file
 const ALEHeadingWord = "Heading"
 
