600b69e3d5032bedb06a8258ca9616d7ec5794d8
Sebastian Reategui <seb.reategui@gmail.com>
2025-03-01 09:21:01 +1100

    cli

--- go/cli/main.go
+++ go/cli/main.go
@@ -1,23 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"lib-post-interchange/libale"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("cli: Please provide a file path as first argument")
-		return
+	app := &cli.App{
+		Name:  "libale-go-cli",
+		Usage: "A CLI to read and write ALE files",
+		Commands: []*cli.Command{
+			{
+				Name:  "read",
+				Usage: "Read an ALE file",
+				Action: func(c *cli.Context) error {
+					if c.NArg() < 1 {
+						return cli.Exit("please provide a file path as the first argument", 1)
+					}
+					inputFile := c.Args().Get(0)
+					c.App.Writer.Write([]byte("cli: Input file: " + inputFile + "\n"))
+					ale, err := libale.ReadFile(inputFile)
+					if err != nil {
+						return cli.Exit("libale: error reading file: "+err.Error(), 1)
+					}
+					c.App.Writer.Write([]byte("cli: Output ALE: " + ale.String() + "\n"))
+					return nil
+				},
+			},
+			{
+				Name:  "write",
+				Usage: "Write metadata to an ALE file",
+				Action: func(c *cli.Context) error {
+					// Function stub for writing metadata
+					c.App.Writer.Write([]byte("cli: Write command executed\n"))
+					return nil
+				},
+			},
+		},
 	}
-	inputFile := os.Args[1]
-	fmt.Printf("cli: Input file: %s", inputFile)
-	ale, err := libale.ReadFile(inputFile)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("ALE object: %s", ale)
 
+	err := app.Run(os.Args)
+	if err != nil {
+		cli.OsExiter(1)
+	}
 }
